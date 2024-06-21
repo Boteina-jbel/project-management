1c9c8b77d76f4b39cc09789558db5f9eb647bcce
anasBoukhlija <anas.boukhlija@saphirsystems.com>
2024-06-21 23:47:24 +0100

    TODO Portfolio

---
+++ project-management/src/main/java/project/management/dto/Portfolio.java
@@ -0,0 +1,14 @@
+package project.management.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Portfolio {
+
+    private Integer projectCount;
+
+}
